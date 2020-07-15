1cc31dfaca191d80a380247a012bb81f141a4611
oTeun <40315823+oTeun@users.noreply.github.com>
2020-07-15 13:56:57 +0200

    v1
    yes good
    this is good

---
+++ best snipper 2020.py
@@ -0,0 +1,10 @@
+print("do u want to snipe")
+print("1 - yes")
+print("2 - no")
+choice = int(input("wat ur choic"))
+if choice==1:
+    print("brrrrrrrrrrrrrrrrr")
+    print("sniped ez")
+else:
+    print("ok bye")
+input()
